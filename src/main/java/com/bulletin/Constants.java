bdf9066c19e4f57841c02cb5fcf3744ec935d3b9
Geoffroy Weyer <webweyer@gmail.com>
2015-12-08 19:08:22 +0100

    We are un 2015-2016

--- src/main/java/com/bulletin/Constants.java
+++ src/main/java/com/bulletin/Constants.java
@@ -6,8 +6,8 @@ import com.bulletin.outputStream.Trimester;
  * Created by geoffroy on 27/06/15.
  */
 public interface Constants {
-    int FIRST_YEAR = 2014;
-    int SECOND_YEAR = 2015;
-    Trimester CURRENT_TRIMESTER = Trimester.THIRD_TRIMESTER;
+    int FIRST_YEAR = 2015;
+    int SECOND_YEAR = 2016;
+    Trimester CURRENT_TRIMESTER = Trimester.FIRST_TRIMESTER;
     int NUMBER_OF_LINE_ON_FIRST_PAGE = 51;
 }
